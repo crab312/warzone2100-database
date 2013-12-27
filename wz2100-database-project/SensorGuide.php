a08448ae2aefd38144e1d2230e93ee53fa90093e
Vladimir Salnikov <reg312@gmail.com>
2013-12-27 05:35:06 +0400

    Added pages from Old Guide: Early Game Strategy, Sensor&Artillery Guide(unfinished)

---
+++ wz2100-database-project/SensorGuide.php
@@ -0,0 +1,16 @@
+<?php
+ob_start('ob_gzhandler'); //enables compression 
+include("SensorGuide.html");
+?>
+
+<script type='text/javascript'>
+<?php
+//$php_array = array('abc','def','ghi');
+$js_array = array_merge(scandir("data_icons/SupportTurrets"));
+$js_hash_array = array();
+foreach ($js_array as &$value) {
+    $js_hash_array[$value] = 1;
+}
+echo "var icon_files_hash = ". json_encode($js_hash_array) . ";\n";
+?>
+</script>
